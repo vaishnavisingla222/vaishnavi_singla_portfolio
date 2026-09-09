3495b9c5e441e3fafbc9a551ddc50ebd2a9a167c
Vaishnavi Singla <vsingla_be24@thapar.edu.com>
2026-09-09 12:00:13 +0530

    Add active navigation highlighting

--- script.js
+++ script.js
@@ -28,3 +28,37 @@ themeToggle.addEventListener("click", () => {
     }
 
 });
+
+
+const sections = document.querySelectorAll("section, header");
+const navLinks = document.querySelectorAll("nav a");
+window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 200;
+        const sectionHeight = section.clientHeight;
+        if (
+            window.scrollY >= sectionTop &&
+            window.scrollY < sectionTop + sectionHeight
+        ) {
+            current = section.id;
+        }
+
+    });
+
+    if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 10
+    ) {
+        current = "contact";
+    }
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+});
