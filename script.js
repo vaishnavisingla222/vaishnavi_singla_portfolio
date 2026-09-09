fa0ca5c182c818291827853b02f964c438ed6bf5
Vaishnavi Singla <vsingla_be24@thapar.edu.com>
2026-09-09 11:48:32 +0530

    Added theme toggle and Resume

---
+++ script.js
@@ -0,0 +1,30 @@
+const themeToggle = document.getElementById("theme-toggle");
+
+// Check saved theme
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    themeToggle.textContent = "☀️";
+}
+
+// Toggle theme
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+
+        themeToggle.textContent = "☀️";
+
+        localStorage.setItem("theme", "dark");
+
+    } else {
+
+        themeToggle.textContent = "🌙";
+
+        localStorage.setItem("theme", "light");
+
+    }
+
+});
