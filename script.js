fda5bc0876740d142864f3d29a4aadf6761317f7
Vaishnavi Singla <vsingla_be24@thapar.edu.com>
2026-09-15 11:24:13 +0530

    Added DSA section and links to platform

--- script.js
+++ script.js
@@ -62,3 +62,23 @@ window.addEventListener("scroll", () => {
 
     });
 });
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.dataset.target;
+        const count = +counter.innerText;
+
+        const increment = target / 100;
+
+        if(count < target){
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount, 20);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCount();
+});
